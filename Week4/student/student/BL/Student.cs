a5abaa7994173f684c9aa6647a383665329db314
Owais Khubaisi <144408310+codemaestroowais@users.noreply.github.com>
2024-02-25 16:17:30 +0500

    Add files via upload

---
+++ Week4/student/student/BL/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    internal class Student
+    {
+        public string name;
+        public int rollNumber;
+        public float cgpa;
+        public int matricMarks;
+        public int ecatMarks;
+        public int fscMarks;
+        public string homeTown;
+        public bool isHostellite;
+        public bool isTakingScholarship;
+
+        public Student()
+        {
+            matricMarks = 1100;
+            ecatMarks = 300;
+            fscMarks = 1001;
+        }
+
+        public float calculateMerit()
+        {
+            return ((fscMarks * 0.05f) + (ecatMarks * 0.1f));
+        }
+
+        public bool isHostelite()
+        {
+            if(calculateMerit() > 80)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+    }
+
+
+}
---
+++ Week4/student/student/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+           Console.WriteLine( student.calculateMerit());
+            Console.WriteLine(student.isHostelite());
+
+
+        }
+    }
+}
